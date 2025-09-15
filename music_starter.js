b8a9cc77b94c177912b52dd8431cd268886c42e7
Josh-Clarke9 <clarkejosh2@myvuw.ac.nz>
2025-09-15 14:54:12 +1200

    Refining
    Change text
    changing spiral animation to rotate with bass and resize to vocals.

--- music_starter.js
+++ music_starter.js
@@ -1,9 +1,7 @@
 
-
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   background(10, 20, 30)
-
 
 //variables
 rectMode (CENTER);
@@ -23,33 +21,34 @@ translate(width / 2, height / 2)
 for (var i = 0; i < 200; i++) {
   push()
 
-  rotate(sin(frameCount + i * 4) * 100)
+  rotate(sin(frameCount + i * 4) * bass)
 
-  rect(0, 0, 600 - i * 3, 600 - i * 3, 200 - i)
+  rect(0, 0, 600 - i * 3, 600 - i * 3+vocal*2, 200 - i+vocal*2)
 
   pop()
 }
 
-
 //drawings
-stroke(50,70,5);
-strokeWeight(10);
-fill(0,50,0);
-ellipse(vocalPos-800,midY/12,50 ,50);
-ellipse(vocalPos-400,midY/12,50,50);
-ellipse(vocalPos,midY/12,50,50);
+{push()
+rotate(sin(frameCount + i * 4) * 100)
+stroke(100,150,60);
+strokeWeight(5);
+fill(200,345,60);
+ellipse(vocalPos-800,midY/12,bass,bass);
+ellipse(vocalPos-400,midY/12,bass,bass);
+ellipse(vocalPos,midY/12,bass,bass);
 
 stroke(100,150,60);
-strokeWeight(10);
+strokeWeight(5);
 fill(200,345,60)
-ellipse(ellipsePos-400,midY/24,50,50);
-ellipse(ellipsePos-400,midY/12,50,50);
-ellipse(ellipsePos-400,midY/6,50,50);
-
+ellipse(ellipsePos-400,midY/24,drum,drum);
+ellipse(ellipsePos-400,midY/12,drum,drum);
+ellipse(ellipsePos-400,midY/6,drum,drum);
+pop()}
    stroke(100,150,60);
    strokeWeight(3);
    fill(154,205,50);
-   textFont('Verdana'); // please use CSS safe fonts
+   textFont('Impact'); // please use CSS safe fonts
    rectMode(CENTER)
    textSize(8);
     textAlign(CENTER);
